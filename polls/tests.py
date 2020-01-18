b6dfa68aaac18bd1603fc4e6f7e9b55ae35814a2
Camilo Caquimbo <ingcamiloc@gmail.com>
2020-01-18 10:27:37 -0500

    Init commit

---
+++ polls/tests.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+from django.utils import timezone
+import datetime
+from django.utils import datetime_safe
+
+from .models import Question
+
+# Create your tests here.
+
+
+class QuestionModelTest(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(
+        ), False, 'Paila no funciona, está validando las fechas mayores a 1 día como recientes')
+
+    def test_was_published_recently_with_old_question(self):
+      time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+      old_question = Question(pub_date=time)
+      self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+      time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+      recent_question = Question(pub_date=time)
+      self.assertIs(recent_question.was_published_recently(), True)
